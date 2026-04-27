b2afda9b65423a069969ba988f666df1840ea98d
fathirmareto-ui <fathirmareto@gmail.com>
2026-04-27 19:00:34 +0700

    tambah anggota

---
+++ app/Http/Controllers/AnggotaController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Anggota;
+use Illuminate\Http\Request;
+
+class AnggotaController extends Controller
+{
+    public function index()
+    {
+        $anggotas = Anggota::all();
+        return view('anggota.index', compact('anggotas'));
+    }
+
+    public function create()
+    {
+        return view('anggota.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required',
+            'email' => 'required|email',
+            'no_hp' => 'required',
+            'alamat' => 'required',
+        ]);
+
+        Anggota::create($request->all());
+
+        return redirect()->route('anggota.index')
+            ->with('success', 'Data berhasil ditambah');
+    }
+
+    public function edit($id)
+    {
+        $anggota = Anggota::findOrFail($id);
+        return view('anggota.edit', compact('anggota'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $anggota = Anggota::findOrFail($id);
+        $anggota->update($request->all());
+
+        return redirect()->route('anggota.index')
+            ->with('success', 'Data berhasil diupdate');
+    }
+
+    public function destroy($id)
+    {
+        $anggota = Anggota::findOrFail($id);
+        $anggota->delete();
+
+        return redirect()->route('anggota.index')
+            ->with('success', 'Data berhasil dihapus');
+    }
+}
